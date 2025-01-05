91e4d78fa03145f8cb7eb3fb8bba67e7bb0a267e
Areian <iantyrho13@naver.com>
2025-01-05 12:04:34 +0900

    2025-01-05 12'04

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,3 +1,5 @@
+import "./Main.css"
+
 // JSX 주의사항
 // 1. 중괄호 내부에는 자바스크립트 표현식만 넣을 수 있다
 // if, for 같은건 못쓴다는 얘기다.
@@ -12,18 +14,12 @@ const Main = () => {
     };
 
     if (user.isLogin) {
-        return <div>logout</div>;
+        return <div className="logout">logout</div>;
     } else {
         return <div>login</div>;
     }
     
-    // return (
-    //     <>
-    //         {
-    //         user.isLogin ? <div>logout</div> : <div>login</div>
-    //         }
-    //     </>
-    // );
+    // return user.isLogin ? <div>logout</div> : <div>login</div>;
 };
 
 export default Main;
