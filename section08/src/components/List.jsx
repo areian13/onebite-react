4b99577ff70539dbae8aec8cfd66cf8447546091
Areian <iantyrho13@naver.com>
2025-01-08 02:17:01 +0900

    2025-01-08 02'17

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,15 +1,34 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
-const List = () => {
+const List = ({ todos }) => {
+    const [search, setSearch] = useState("");
+
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    }
+
+    const getFliteredData = () => {
+        if (search === "") {
+            return todos;
+        }
+        return todos.filter((todo) => todo.content.toLowerCase().includes(search.toLowerCase()));
+    }
+
+    const filteredTodos = getFliteredData(); 
+
     return (
         <div className="List">
-            <h4>Todo List🌱</h4>
-            <input placeholder="검색어를 입력하세요" />
+            <h4>Todo List 🌱</h4>
+            <input
+                value={search}
+                onChange={onChangeSearch}
+                placeholder="검색어를 입력하세요" />
             <div className="todos_wrapper">
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
+                {filteredTodos.map((todo) => {
+                    return <TodoItem key={todo.id} {...todo} />
+                })}
             </div>
         </div>
     );
